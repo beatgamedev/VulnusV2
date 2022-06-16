f40a8760570c9739262317c8a3ae50888d1c8f11
jayden <46307433+krmeet@users.noreply.github.com>
2022-06-16 14:48:40 +0100

    map

--- scripts/content/Map.cs
+++ scripts/content/Map.cs
@@ -1,26 +1,57 @@
 using Godot;
 using System;
 using System.Collections.Generic;
+using Newtonsoft.Json;
 
-[Serializable]
+[Serializable, JsonObject(MemberSerialization.OptIn)]
 public class Map
 {
 	public static int LatestFormat = 2;
+	[JsonProperty("_version")]
 	public int FormatVersion;
-	public string Artist;
-	public string Title;
-	public List<string> DifficultyPaths;
 	[NonSerialized]
+	public string Path;
+	[JsonProperty("_artist")]
+	public string Artist;
+	[JsonProperty("_title")]
+	public string Title;
+	[JsonProperty("_difficulties")]
+	public List<string> _difficulties;
 	public List<Difficulty> Difficulties;
+	[JsonProperty("_mappers")]
 	public List<string> Mappers;
+	[JsonProperty("_music")]
 	public string Music;
-	public struct Note {
+	[Serializable, JsonObject]
+	public class Note
+	{
 		public float X;
 		public float Y;
 		public float T;
 	}
-	public struct Difficulty {
+	[Serializable, JsonObject(MemberSerialization.OptIn)]
+	public class Difficulty
+	{
+		[JsonProperty("_name")]
 		public string Name;
+		[JsonIgnore]
+		public string Path;
+		[JsonIgnore]
+		public MapData Data;
+		public void Load(Map map)
+		{
+			if (Data != null) return;
+			var file = new File();
+			file.Open(map.Path + "/" + Path, File.ModeFlags.Read);
+			var data = JsonConvert.DeserializeObject<MapData>(file.GetAsText());
+			this.Data = data;
+			file.Close();
+		}
+	}
+	[JsonObject]
+	public class MapData : Difficulty
+	{
+		[JsonProperty("_notes")]
 		public List<Note> Notes;
 	}
 }
