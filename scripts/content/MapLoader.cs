384f5778ff04c9b6387f0aa60b9c8bccd80d2c4a
jayden <46307433+krmeet@users.noreply.github.com>
2022-06-16 16:18:10 +0100

    maploader fixes

--- scripts/content/MapLoader.cs
+++ scripts/content/MapLoader.cs
@@ -10,6 +10,7 @@ public static class MapLoader
 	public static List<Map> LoadedMaps = new List<Map>();
 	public static bool LoadMapsFromDirectory(string directory)
 	{
+		GD.Print("Loading maps from " + directory);
 		// Get a list of cached maps
 		var cachePath = directory.PlusFile(".cache");
 		var cacheDir = new Directory();
@@ -22,13 +23,18 @@ public static class MapLoader
 		var cacheFileName = cacheDir.GetNext();
 		while (cacheFileName != "")
 		{
-			caches.Add(cacheFileName);
+			if (cacheDir.CurrentIsDir())
+			{
+				caches.Add(cacheFileName);
+				GD.Print("Found cache: " + cacheFileName);
+			}
 			cacheFileName = cacheDir.GetNext();
 		}
 		// Get a list of existing maps
 		var hashes = new List<string>();
 		var mapsDir = new Directory();
 		mapsDir.Open(directory);
+		mapsDir.ListDirBegin(true, true);
 		var mapFileName = mapsDir.GetNext();
 		var mapFile = new File();
 		while (mapFileName != "")
@@ -36,9 +42,11 @@ public static class MapLoader
 			if (mapFileName.Extension() == "vmap")
 			{
 				var hash = mapFile.GetMd5(directory + "/" + mapFileName);
+				GD.Print("Found map: " + hash);
 				hashes.Add(hash);
 				if (!caches.Contains(hash)) // If the map isn't already cached, do extraction stuff
 				{
+					GD.Print("New cache added: " + hash);
 					mapFile.Open(directory + "/" + mapFileName, File.ModeFlags.Read);
 					var stream = new MemoryStream(mapFile.GetBuffer((long)mapFile.GetLen()));
 					ZipArchive zip = new ZipArchive(stream, ZipArchiveMode.Read);
@@ -46,15 +54,22 @@ public static class MapLoader
 				}
 				LoadedMaps.Add(Map.LoadFromPath(cachePath.PlusFile(hash))); // Load map from cache
 			}
+			else
+			{
+				GD.Print(mapFileName + " is not a map file");
+			}
+			mapFileName = mapsDir.GetNext();
 		}
 		// Delete old caches
 		foreach (string hash in caches)
 		{
 			if (!hashes.Contains(hash))
 			{
+				GD.Print("Old cache deleted: " + hash);
 				System.IO.Directory.Delete(cachePath.PlusFile(hash), true);
 			}
 		}
+		if (mapFile.IsOpen()) mapFile.Close();
 		return true;
 	}
 }
