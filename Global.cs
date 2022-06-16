b068c59936327c8a2809e2a451265275d42fb8e1
jayden <46307433+krmeet@users.noreply.github.com>
2022-06-16 15:47:50 +0100

    try load maps on startup

--- Global.cs
+++ Global.cs
@@ -4,18 +4,26 @@ using System;
 public class Global : Node
 {
 	public static Global Instance;
-	public Node CurrentScene {get; set;}
-	
-	public override void _Ready() {
+	public Node CurrentScene { get; set; }
+
+	public override void _Ready()
+	{
 		Instance = this;
 		Viewport root = GetTree().Root;
-		CurrentScene = root.GetChild(root.GetChildCount()-1);
+		CurrentScene = root.GetChild(root.GetChildCount() - 1);
+
+		var start = OS.GetTicksUsec();
+		MapLoader.LoadMapsFromDirectory(OS.GetUserDataDir().PlusFile("maps"));
+		var end = OS.GetTicksUsec();
+		GD.Print("Took " + (end - start) / 1000 + "ms to load maps");
 	}
-	
-	public void GotoScene(string path) {
+
+	public void GotoScene(string path)
+	{
 		CallDeferred(nameof(DeferredGotoScene), path);
 	}
-	private void DeferredGotoScene(string path) {
+	private void DeferredGotoScene(string path)
+	{
 		CurrentScene.Free();
 		var nextScene = (PackedScene)GD.Load(path);
 		CurrentScene = nextScene.Instance();
