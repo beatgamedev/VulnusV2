46aa447610a322da3dc34c783b96aedf2abc99e9
jayden <46307433+krmeet@users.noreply.github.com>
2022-07-22 16:13:22 +0100

    working menu buttons

--- scripts/menu/MainMenu.cs
+++ scripts/menu/MainMenu.cs
@@ -1,7 +1,7 @@
 using Godot;
 using System;
 
-public class MainMenuController : Control
+public class MainMenu : Control
 {
 	private Button[] Tabs;
 	private Control[] Views;
@@ -10,19 +10,17 @@ public class MainMenuController : Control
 		var viewContainer = GetNode<Control>("ViewContainer");
 		Control[] views = { viewContainer.GetNode<Control>("Play"), viewContainer.GetNode<Control>("Account"), viewContainer.GetNode<Control>("Settings") };
 		Views = views;
-		var tabBtns = GetNode<Control>("BottomBar/HBoxContainer");
+		var tabBtns = GetNode<Control>("BottomBar/Buttons");
 		Button[] tabs = { tabBtns.GetNode<Button>("Play"), tabBtns.GetNode<Button>("Account"), tabBtns.GetNode<Button>("Settings") };
 		Tabs = tabs;
-		for (var i = 0; i < tabs.Length; i++)
+		for (var i = 0; i < Tabs.Length; i++)
 		{
-			GD.Print(i);
-			tabs[i].Connect("pressed", this, nameof(TabPress), new Godot.Collections.Array() { i });
+			Tabs[i].Connect("pressed", this, "TabPress", new Godot.Collections.Array() { i });
 		}
 		TabPress(0);
 	}
 	public void TabPress(int btn)
 	{
-		GD.Print("TabPress: " + btn);
 		for (int i = 0; i < Tabs.Length; i++)
 		{
 			Tabs[i].Pressed = i == btn;
