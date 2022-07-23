16b56a6038329c9bf97fa8dfdf3ad37e1db070f8
jayden <46307433+krmeet@users.noreply.github.com>
2022-07-24 00:48:42 +0100

    add Play.cs to menu

--- scripts/menu/Play.cs
+++ scripts/menu/Play.cs
@@ -3,4 +3,11 @@ using System;
 
 public class Play : Control
 {
+	private Control mapList;
+	private Control mapDetail;
+	public override void _Ready()
+	{
+		mapList = GetNode<Control>("MapList");
+		mapDetail = GetNode<Control>("MapDetail");
+	}
 }
