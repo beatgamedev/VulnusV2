6f4146f469a3f602f41da353fb885cf626da88e2
jayden <46307433+krmeet@users.noreply.github.com>
2022-08-10 21:30:21 +0100

    make singleplayer button work

--- scripts/menu/MainMenu.cs
+++ scripts/menu/MainMenu.cs
@@ -8,6 +8,8 @@ public class MainMenu : View
 		MenuHandler menu = GetParent<Control>().GetParent<MenuHandler>();
 		var sidebar = GetNode<Control>("Sidebar");
 		var buttons = sidebar.GetNode<Control>("Buttons");
+		var singleBtn = buttons.GetNode<Button>("Singleplayer");
+		singleBtn.Connect("pressed", menu, nameof(MenuHandler.GoTo), new Godot.Collections.Array(1));
 		var optionsBtn = buttons.GetNode<Button>("Options");
 		optionsBtn.Connect("pressed", Global.Instance.Overlays["Options"], nameof(Options.Open));
 	}
