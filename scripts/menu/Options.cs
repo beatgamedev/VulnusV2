1516a48a2c31297004750f02ad724960c7a1a1b6
haawwkeye <connorbishop53@gmail.com>
2022-08-11 14:25:49 -0400

    Options fix

--- scripts/menu/Options.cs
+++ scripts/menu/Options.cs
@@ -3,7 +3,6 @@ using System;
 
 public class Options : View
 {
-	private bool active => IsActive;
 	private Tween tween;
 	private bool opening = false;
 	public override void _Ready()
@@ -18,26 +17,27 @@ public class Options : View
 	{
 		if (Input.IsActionJustPressed("options"))
 		{
-			SetActive(!active);
+			SetActive(!IsActive);
 		}
 	}
-	public override void OnShow()
+	public override async void OnShow()
 	{
-		if (this.Visible)
-			return;
 		opening = true;
 		this.Visible = true;
 		tween.InterpolateProperty(this, "modulate:a", 0, 1, 0.15f, Tween.TransitionType.Sine);
 		tween.InterpolateProperty(this, "rect_scale", new Vector2(0.8f, 0.8f), new Vector2(1, 1), 0.2f, Tween.TransitionType.Sine, Tween.EaseType.Out);
 		tween.Start();
+		await ToSignal(tween, "tween_all_completed");
+		IsActive = true;
 	}
-	public override void OnHide()
+	public override async void OnHide()
 	{
-		if (!this.Visible)
-			return;
 		opening = false;
 		tween.InterpolateProperty(this, "modulate:a", 1, 0, 0.15f, Tween.TransitionType.Sine);
 		tween.InterpolateProperty(this, "rect_scale", new Vector2(1, 1), new Vector2(0.9f, 0.9f), 0.2f, Tween.TransitionType.Sine, Tween.EaseType.Out);
 		tween.Start();
+		await ToSignal(tween, "tween_all_completed");
+		IsActive = false;
+		this.Visible = false;
 	}
 }
