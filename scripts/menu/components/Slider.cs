ac13e6a5cdb0589dfbb2d753b9a0e3e4c61f1fd2
jayden <46307433+krmeet@users.noreply.github.com>
2022-08-18 01:25:51 +0100

    add "Slider"

---
+++ scripts/menu/components/Slider.cs
@@ -0,0 +1,39 @@
+using Godot;
+using System;
+
+public class Slider : Control
+{
+	[Export]
+	public string Label = "";
+	[Export]
+	public float Value = 0;
+	[Export]
+	public float Max = 100;
+	[Export]
+	public float Min = 0;
+	[Export]
+	public float Step = 1;
+	public override void _Ready()
+	{
+		GetNode<Label>("Label").Text = $"{Label} ({Value})";
+		GetNode<Godot.Slider>("Slider").Value = Value;
+		GetNode<Godot.Slider>("Slider").MaxValue = Max;
+		GetNode<Godot.Slider>("Slider").MinValue = Min;
+		GetNode<Godot.Slider>("Slider").Step = Step;
+		GetNode<Godot.Slider>("Slider").Connect("value_changed", this, nameof(OnValueChanged));
+	}
+	public void SetValue(float value)
+	{
+		GetNode<Label>("Label").Text = $"{Label} ({value})";
+		Value = value;
+		GetNode<Godot.Slider>("Slider").Value = value;
+	}
+	public void OnValueChanged(float value)
+	{
+		GetNode<Label>("Label").Text = $"{Label} ({value})";
+		Value = value;
+		EmitSignal(nameof(ValueChanged), value);
+	}
+	[Signal]
+	public delegate void ValueChanged(float value);
+}
