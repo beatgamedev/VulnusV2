65c640de1f088cedab742e723e18b9a54c86302c
jayden <46307433+krmeet@users.noreply.github.com>
2022-08-26 14:13:54 +0100

    change how volume is updated

--- scripts/menu/components/options/Volume.cs
+++ scripts/menu/components/options/Volume.cs
@@ -7,22 +7,17 @@ public class Volume : Control
 	{
 		Slider master = GetNode<Slider>("Master");
 		master.SetValue(Settings.Volume[0]);
-		master.Connect("ValueChanged", this, nameof(OnValueChanged));
+		master.Connect("ValueChanged", this, nameof(OnValueChanged), new Godot.Collections.Array(0));
 		Slider music = GetNode<Slider>("Music");
 		music.SetValue(Settings.Volume[1]);
-		music.Connect("ValueChanged", this, nameof(OnValueChanged));
+		music.Connect("ValueChanged", this, nameof(OnValueChanged), new Godot.Collections.Array(1));
 		Slider sfx = GetNode<Slider>("SFX");
 		sfx.SetValue(Settings.Volume[2]);
-		sfx.Connect("ValueChanged", this, nameof(OnValueChanged));
+		sfx.Connect("ValueChanged", this, nameof(OnValueChanged), new Godot.Collections.Array(2));
 	}
-	public void OnValueChanged(float value)
+	public void OnValueChanged(float value, int index)
 	{
-		Slider master = GetNode<Slider>("Master");
-		Settings.Volume[0] = (int)master.Value;
-		Slider music = GetNode<Slider>("Music");
-		Settings.Volume[1] = (int)music.Value;
-		Slider sfx = GetNode<Slider>("SFX");
-		Settings.Volume[2] = (int)sfx.Value;
+		Settings.Volume[index] = (int)value;
 		Settings.UpdateSettings(false);
 	}
 }
