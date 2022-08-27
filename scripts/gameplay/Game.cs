ebeadc470313478f340772c7c23ed4eaaecf3439
jayden <46307433+krmeet@users.noreply.github.com>
2022-08-28 00:29:42 +0100

    do score stuff (no hp)

--- scripts/gameplay/Game.cs
+++ scripts/gameplay/Game.cs
@@ -41,11 +41,36 @@ public class Game : Spatial
 
 		SyncManager.SetStream(LoadedMap.LoadAudio());
 		SyncManager.Connect("Ended", this, nameof(GameEnded));
+
+		NoteManager.Connect("NoteHit", this, nameof(OnNoteHit));
+		NoteManager.Connect("NoteMiss", this, nameof(OnNoteMiss));
 	}
 	public override void _PhysicsProcess(float delta)
 	{
 		if (Input.IsActionJustPressed("skip") && SyncManager.CanSkip())
 			SyncManager.AttemptSkip();
+	}
+	public void OnNoteHit(Note note)
+	{
+		Score.Points += 25 * Score.Multiplier;
+		Score.Miniplier += 1;
+		if (Score.Miniplier >= 8)
+		{
+			Score.Miniplier = 0;
+			Score.Multiplier = Mathf.Min(8, Score.Multiplier + 1);
+		}
+		Score.Combo += 1;
+		if (Score.Combo > Score.HighestCombo)
+			Score.HighestCombo = Score.Combo;
+		Score.Total += 1;
+	}
+	public void OnNoteMiss(Note note)
+	{
+		Score.Miniplier = 0;
+		Score.Multiplier = Mathf.Max(1, Score.Multiplier - 1);
+		Score.Combo = 0;
+		Score.Misses += 1;
+		Score.Total += 1;
 	}
 	public void GameEnded()
 	{
--- scripts/gameplay/Score.cs
+++ scripts/gameplay/Score.cs
@@ -8,6 +8,6 @@ public class Score
 	public int Total = 0;
 	public int Combo = 0;
 	public int HighestCombo = 0;
-	public int Multiplier = 0;
+	public int Multiplier = 1;
 	public int Miniplier = 0;
 }
