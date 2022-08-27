90ccc6b71534da5d867cf59892efd630b5b3dde4
jayden <46307433+krmeet@users.noreply.github.com>
2022-08-27 17:36:53 +0100

    add a quit button lol

--- scripts/menu/MainMenu.cs
+++ scripts/menu/MainMenu.cs
@@ -13,5 +13,11 @@ public class MainMenu : View
 		singleBtn.Connect("pressed", menu, nameof(MenuHandler.GoTo), new Godot.Collections.Array(1));
 		var optionsBtn = buttons.GetNode<Button>("Options");
 		optionsBtn.Connect("pressed", Global.Instance.Overlays["Options"], nameof(Options.SetActive), new Godot.Collections.Array(true));
+		var exitBtn = buttons.GetNode<Button>("Quit");
+		exitBtn.Connect("pressed", this, nameof(Quit));
+	}
+	public void Quit()
+	{
+		GetTree().Quit(0);
 	}
 }
