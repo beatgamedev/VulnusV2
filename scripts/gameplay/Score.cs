d196d77779771d54fa9572324910988293affe48
jayden <46307433+krmeet@users.noreply.github.com>
2022-08-28 00:16:52 +0100

    why did i make these that type

--- scripts/gameplay/Score.cs
+++ scripts/gameplay/Score.cs
@@ -3,11 +3,11 @@ using System;
 
 public class Score
 {
-	public uint Points = 0;
-	public uint Misses = 0;
-	public uint Total = 0;
-	public uint Combo = 0;
-	public uint HighestCombo = 0;
+	public int Points = 0;
+	public int Misses = 0;
+	public int Total = 0;
+	public int Combo = 0;
+	public int HighestCombo = 0;
 	public int Multiplier = 0;
 	public int Miniplier = 0;
 }
