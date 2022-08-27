c6f14867859527f76dd9ed71a2b3702f4f49cd25
jayden <46307433+krmeet@users.noreply.github.com>
2022-08-27 22:17:25 +0100

    add skipping

--- scripts/gameplay/NoteManager.cs
+++ scripts/gameplay/NoteManager.cs
@@ -13,6 +13,9 @@ public class NoteManager : Node
 	public NoteRenderer NoteRenderer;
 	public SyncManager SyncManager;
 
+	public Note NextNote;
+	public Note LastNote;
+
 	public override void _Ready()
 	{
 		Game = GetParent<Game>();
@@ -26,6 +29,8 @@ public class NoteManager : Node
 			Notes.Add(new Note(noteData.X * 2, noteData.Y * 2, noteData.T, i));
 			i++;
 		}
+		if (Notes.Count > 0)
+			NextNote = Notes[0];
 	}
 	public override void _Process(float delta)
 	{
@@ -37,8 +42,35 @@ public class NoteManager : Node
 	}
 	public override void _PhysicsProcess(float delta)
 	{
-
+		var approachTime = Settings.ApproachTime;
+		var visibleNotes = Notes.FindAll(note => note.CalculateTime(SyncManager.NoteTime, approachTime) <= 0f);
+		foreach (Note note in visibleNotes)
+		{
+			bool didHitreg = false;
+			if (note.IsTouching(Camera.ClampedCursorPosition))
+			{
+				EmitSignal(nameof(NoteHit), note);
+				didHitreg = true;
+			}
+			if (!note.Hit && !note.InHitWindow(SyncManager.NoteTime, true))
+			{
+				EmitSignal(nameof(NoteMiss), note);
+				didHitreg = true;
+			}
+			if (didHitreg)
+			{
+				note.Hit = true;
+				if (Notes.Count > note.Index)
+					NextNote = Notes[(int)note.Index + 1];
+				else
+					NextNote = null;
+				LastNote = note;
+			}
+			continue;
+		}
 	}
 	[Signal]
-	public delegate void NotesFinished();
+	public delegate void NoteHit(Note note);
+	[Signal]
+	public delegate void NoteMiss(Note note);
 }
--- scripts/gameplay/SyncManager.cs
+++ scripts/gameplay/SyncManager.cs
@@ -4,43 +4,35 @@ using System;
 public class SyncManager : Node
 {
 	public Game Game;
-	public GameCamera Camera;
+
+	public NoteManager NoteManager;
 
 	public AudioStreamPlayer AudioPlayer;
 
 	public double NoteTime;
 	public double SongTime;
+	public double SkippedTime;
 	public bool SongPlaying;
 	public double SongPlayingAt;
 	public double SongPlayingOffset;
 
 	public override void _Ready()
 	{
+		Game = GetParent<Game>();
+
+		NoteManager = Game.GetNode<NoteManager>("NoteManager");
+
 		AudioPlayer = GetNode<AudioStreamPlayer>("AudioPlayer");
+		AudioPlayer.Connect("finished", this, nameof(AudioEnded));
+
 		NoteTime = 0f;
 		SongTime = -1f;
-	}
-	private double GetTimeSeconds()
-	{
-		return OS.GetTicksUsec() / 1000000.0d;
-	}
-	private double GetAudioDelay()
-	{
-		return AudioServer.GetTimeToNextMix() + AudioServer.GetOutputLatency();
-	}
-	public void PlayAudio()
-	{
-		SongPlayingAt = GetTimeSeconds();
-		SongPlayingOffset = GetAudioDelay();
-		AudioPlayer.Play((float)SongTime);
-	}
-	public void SetStream(AudioStream stream)
-	{
-		AudioPlayer.Stop();
-		AudioPlayer.Stream = stream;
+		SkippedTime = 0;
 	}
 	public override void _Process(float delta)
 	{
+		if (Game.Ended)
+			return;
 		if (!SongPlaying)
 		{
 			SongTime += delta;
@@ -53,7 +45,7 @@ public class SyncManager : Node
 		else
 		{
 			var songTime = GetTimeSeconds() - SongPlayingAt;
-			SongTime = Math.Max(0.0, songTime - SongPlayingOffset);
+			SongTime = Math.Max(0.0, songTime - SongPlayingOffset) + SkippedTime;
 			var difference = SongTime - (AudioPlayer.GetPlaybackPosition() + AudioServer.GetTimeSinceLastMix());
 			if (difference > 0.002)
 			{
@@ -64,5 +56,58 @@ public class SyncManager : Node
 		if (SongPlaying && !AudioPlayer.Playing)
 			AudioPlayer.Play();
 		NoteTime = SongTime;
+	}
+	public override void _Input(InputEvent @event)
+	{
+		if (!(@event is InputEventAction))
+			return;
+		var input = @event as InputEventAction;
+		if (input.Action == "skip")
+		{
+			var skippableTime = SkippableTime();
+			if (CanSkip())
+			{
+				SkippedTime += skippableTime;
+				AudioPlayer.Seek((float)(SongTime + skippableTime));
+				SongTime += skippableTime;
+			}
+		}
+	}
+	public bool CanSkip()
+	{
+		if (NoteManager.LastNote == null || NoteManager.NextNote == null)
+			return SkippableTime() >= 2f;
+		return SkippableTime() >= 2f && (NoteManager.NextNote.T - NoteManager.LastNote.T) > 5f;
+	}
+	public double SkippableTime()
+	{
+		if (NoteManager.NextNote == null)
+			return (double)AudioPlayer.Stream.GetLength() - NoteTime - 1f;
+		return NoteManager.NextNote.T - NoteTime - 1f;
+	}
+	[Signal]
+	public delegate void Ended();
+	public void AudioEnded()
+	{
+		EmitSignal(nameof(Ended));
+	}
+	public void SetStream(AudioStream stream)
+	{
+		AudioPlayer.Stop();
+		AudioPlayer.Stream = stream;
+	}
+	private void PlayAudio()
+	{
+		SongPlayingAt = GetTimeSeconds();
+		SongPlayingOffset = GetAudioDelay();
+		AudioPlayer.Play((float)SongTime);
+	}
+	private double GetTimeSeconds()
+	{
+		return OS.GetTicksUsec() / 1000000.0d;
+	}
+	private double GetAudioDelay()
+	{
+		return AudioServer.GetTimeToNextMix() + AudioServer.GetOutputLatency();
 	}
 }
