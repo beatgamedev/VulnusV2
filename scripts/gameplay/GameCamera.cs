2dc47aedac386b9aedebf4ac992538b1c44e4a2c
jayden <46307433+krmeet@users.noreply.github.com>
2022-08-27 11:10:19 +0100

    camera stuff

--- scripts/gameplay/GameCamera.cs
+++ scripts/gameplay/GameCamera.cs
@@ -6,8 +6,13 @@ public class GameCamera : Godot.Camera
 	public float Yaw = 0f;
 	public float Pitch = 0f;
 	public Vector2 CursorPosition = new Vector2();
+	public Vector2 ClampedCursorPosition = new Vector2();
+	public Spatial Cursor = null;
+	public Spatial GhostCursor = null;
+	private static float clamp = (6f - 0.525f) / 2f;
 	public override void _EnterTree()
 	{
+		Settings.UpdateSettings(true);
 		Yaw = 0f;
 		Pitch = 0f;
 		CursorPosition = new Vector2();
@@ -17,23 +22,33 @@ public class GameCamera : Godot.Camera
 	{
 		Input.SetMouseMode(Input.MouseMode.Visible);
 	}
-	public override void _Process(float delta)
-	{
-		Rotation = new Vector3(Pitch, Yaw, 0);
-		Translation = (new Vector3(CursorPosition.x, CursorPosition.y, 0) / 4f) - (Transform.basis.z / 2f);
-	}
 	public override void _Input(InputEvent @event)
 	{
+		if (!(@event is InputEventMouseMotion))
+			return;
 		var input = @event as InputEventMouseMotion;
 		var relative = input.Relative * Settings.MouseSensitivity;
 		if (Settings.CameraMode == 0)
 		{
-			Yaw = Mathf.Wrap(Yaw + relative.x, -180f, 180f);
-			Pitch = Mathf.Wrap(Pitch + relative.y, -90f, 90f);
+			Yaw = Mathf.Wrap(Yaw - relative.x, -180f, 180f);
+			Pitch = Mathf.Clamp(Pitch - relative.y, -90f, 90f);
+			var position = new Vector2(Translation.x, Translation.y);
+			var look = Transform.basis.z;
+			CursorPosition = position + new Vector2(-Mathf.Abs(look.z), look.y) * Mathf.Abs(position.x) / look.z;
+			// v74 = v70.Position + Vector3.new(-math.abs(v70.LookVector.X), v70.LookVector.Y, v70.LookVector.Z) * math.abs(math.abs(v70.Position.X - l__Screen__49.Position.X + v22.Size.X / 2) / v70.LookVector.X);
 		}
 		else
 		{
-			CursorPosition += new Vector2(relative.x, relative.y);
+			Yaw = 0f;
+			Pitch = 0f;
+			CursorPosition += new Vector2(relative.x, -relative.y) * 0.065f;
 		}
+		ClampedCursorPosition = new Vector2(Mathf.Clamp(CursorPosition.x, -clamp, clamp), Mathf.Clamp(CursorPosition.y, -clamp, clamp));
+		Rotation = new Vector3(Mathf.Deg2Rad(Pitch), Mathf.Deg2Rad(Yaw), 0);
+		Translation = new Vector3(0, 0, 7) + new Vector3(ClampedCursorPosition.x, ClampedCursorPosition.y, 0) / 4f - Transform.basis.z / 2f;
+		if (Cursor != null)
+			Cursor.Translation = new Vector3(ClampedCursorPosition.x, ClampedCursorPosition.y, 0);
+		if (GhostCursor != null)
+			GhostCursor.Translation = new Vector3(CursorPosition.x, CursorPosition.y, 0);
 	}
 }
