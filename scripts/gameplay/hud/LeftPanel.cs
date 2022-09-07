8625094d12f34c957453c99ce80ca3da58a8e781
jayden <46307433+krmeet@users.noreply.github.com>
2022-09-07 22:13:36 +0100

    divide 0

--- scripts/gameplay/hud/LeftPanel.cs
+++ scripts/gameplay/hud/LeftPanel.cs
@@ -12,7 +12,7 @@ public class LeftPanel : Panel
 	{
 		if (total < 1)
 			total = 1;
-		double accuracy = (total - misses) / total;
+		double accuracy = total > 0 ? (total - misses) / total : 0;
 		GetNode<Label>("Accuracy").Text = String.Format("{0:.##}%", accuracy * 100);
 		GetNode<Label>("Rank").Text = Score.GetRankForAccuracy(accuracy);
 	}
