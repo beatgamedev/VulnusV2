59ea59b8dd1c72a99653e12c2a1090cb9e59300e
jayden <46307433+krmeet@users.noreply.github.com>
2022-09-07 17:14:52 +0100

    update rendersettings

--- scripts/menu/components/options/RenderSettings.cs
+++ scripts/menu/components/options/RenderSettings.cs
@@ -5,11 +5,10 @@ public class RenderSettings : Control
 {
 	public override void _Ready()
 	{
-		var usedDriver = OS.GetCurrentVideoDriver();
-		var projDriver = (string)ProjectSettings.GetSetting("rendering/quality/driver/driver_name") == "GLES2" ? OS.VideoDriver.Gles2 : OS.VideoDriver.Gles3;
-		GetNode<CheckButton>("VSync").Disabled = (usedDriver != projDriver) || !OS.HasFeature("pc");
-		GetNode<CheckButton>("VSync").Pressed = (usedDriver == OS.VideoDriver.Gles2) || (projDriver == OS.VideoDriver.Gles2);
+		GetNode<CheckButton>("VSync").Disabled = !OS.HasFeature("pc");
+		GetNode<CheckButton>("VSync").Pressed = Settings.VSync;
 		GetNode<CheckButton>("VSync").Connect("pressed", this, nameof(OnButtonPressed), new Godot.Collections.Array(0));
+		GetNode<CheckButton>("Debanding").Pressed = Settings.Debanding;
 		GetNode<DecimalInput>("FPS").SetValue(Settings.FPSLimit);
 		GetNode<DecimalInput>("FPS").Connect("ValueChanged", this, nameof(OnValueChanged));
 	}
