345c0bbdc7b8cdbf8c84fa728fa59a11cff51118
jayden <46307433+krmeet@users.noreply.github.com>
2022-09-08 00:34:45 +0100

    add another mp3 header

--- scripts/utility/Audio.cs
+++ scripts/utility/Audio.cs
@@ -31,7 +31,7 @@ public static class AudioHandler
 	{
 		if (bytes.Length < 10) return "unknown";
 		if (bytes[0] == 0x52 && bytes[1] == 0x49 && bytes[2] == 0x46 && bytes[3] == 0x46) return "wav";
-		if ((bytes[0] == 0xFF && bytes[1] == 0xFB) || (bytes[0] == 0x49 && bytes[1] == 0x44 && bytes[2] == 0x33)) return "mp3";
+		if ((bytes[0] == 0xFF && (bytes[1] == 0xFB || (bytes[1] == 0xFA && bytes[2] == 0x90))) || (bytes[0] == 0x49 && bytes[1] == 0x44 && bytes[2] == 0x33)) return "mp3";
 		if (bytes[0] == 0x4F && bytes[1] == 0x67 && bytes[2] == 0x67 && bytes[3] == 0x53) return "ogg";
 		return "unknown";
 	}
