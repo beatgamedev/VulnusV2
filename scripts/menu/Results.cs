7333da889614553cff9b8cd8d4bc65cb9bae845f
jayden <46307433+krmeet@users.noreply.github.com>
2022-09-07 22:24:16 +0100

    make results screen functional

--- scripts/menu/Results.cs
+++ scripts/menu/Results.cs
@@ -5,6 +5,7 @@ public class Results : View
 {
 	public override void _Ready()
 	{
+		base._Ready();
 		if (Game.Score == null)
 			return;
 		var info = GetNode<Control>("Info");
@@ -13,5 +14,16 @@ public class Results : View
 		mapInfo.GetNode<Label>("Title").Text = Game.LoadedMapset.Name;
 		mapInfo.GetNode<Label>("Mappers").Text = Game.LoadedMapset.Mappers;
 		mapInfo.GetNode<Label>("Difficulty").Text = Game.LoadedMap.Name;
+		info.GetNode<Label>("Score").Text = String.Format("{0:n0}", Game.Score.Points);
+		info.GetNode<Label>("Combo").Text = String.Format("{0:n0}", Game.Score.HighestCombo);
+		info.GetNode<Label>("Hits").Text = String.Format("{0:n0}", Game.Score.Total - Game.Score.Misses);
+		info.GetNode<Label>("Misses").Text = String.Format("{0:n0}", Game.Score.Misses);
+		double accuracy = Game.Score.Total > 0 ? (double)(Game.Score.Total - Game.Score.Misses) / (double)Game.Score.Total : 0;
+		info.GetNode<Label>("Accuracy").Text = accuracy > 0 ? String.Format("{0:.##}%", accuracy * 100) : "0%";
+		info.GetNode<Label>("Rank").Text = Score.GetRankForAccuracy(accuracy);
+
+		GetNode<Button>("Retry").Connect("pressed", Global.Instance, nameof(Global.GotoScene), new Godot.Collections.Array("res://scenes/Game.tscn", null));
+		var menuHandler = GetParent().GetParent<MenuHandler>();
+		GetNode<Button>("Return").Connect("pressed", menuHandler, nameof(MenuHandler.GoTo), new Godot.Collections.Array(1));
 	}
 }
