a35420a2db898b7ac6a21405d63edb3eec76fec1
jayden <46307433+krmeet@users.noreply.github.com>
2022-10-29 23:40:31 +0100

    some menu redesign stuff

--- scripts/menu/components/maps/MapDetails.cs
+++ scripts/menu/components/maps/MapDetails.cs
@@ -9,62 +9,11 @@ public class MapDetails : View
 	private MapList mapList;
 	public override void _Ready()
 	{
-		base._Ready();
-		mapList = GetParent().GetNode<MapList>("MapList");
-		mapList.Connect("MapSelected", this, nameof(OnMapSelected));
-		GetNode<OptionButton>("Difficulty").Connect("item_selected", this, nameof(OnDifficultySelected));
-		GetNode<Button>("Play").Connect("pressed", this, nameof(OnPlayPressed));
 		if (Game.Score == null)
 		{
 			SetActive(false);
 			return;
 		}
-		OnMapSelected(Game.LoadedMapset.Hash);
-		OnDifficultySelected(Game.LoadedMapset.Difficulties.IndexOf(Game.LoadedMap));
-	}
-	public void OnPlayPressed()
-	{
-		GetNode<AudioStreamPlayer>("MusicPreview").Stop();
-		Global.Instance.GotoScene("res://scenes/Game.tscn");
-	}
-	public void OnMapSelected(string hash)
-	{
-		var preview = GetNode<AudioStreamPlayer>("MusicPreview");
-		if (hash == null)
-		{
-			preview.Stop();
-			SetActive(false);
-			return;
-		}
-		if (!this.IsActive)
-			SetActive(true);
-		var map = BeatmapLoader.LoadedMaps.Find(m => m.Hash == hash);
-		currentMap = map;
-		GetNode<Button>("Play").Visible = false;
-		GetNode<TextureRect>("Cover").Texture = map.LoadCover();
-		GetNode<Label>("Title").Text = map.Name;
-		GetNode<Label>("Mappers").Text = map.Mappers;
-		GetNode<OptionButton>("Difficulty").Clear();
-		var i = 0;
-		foreach (var difficulty in map.Difficulties)
-		{
-			GetNode<OptionButton>("Difficulty").AddItem(difficulty.Name, i);
-			i++;
-		}
-		GetNode<OptionButton>("Difficulty").Selected = 0;
-		OnDifficultySelected(0);
-		preview.Stream = null;
-		preview.Stream = map.LoadAudio();
-		preview.Play(preview.Stream.GetLength() / 3);
-		Game.LoadedMapset = currentMap;
-	}
-	public void OnDifficultySelected(int idx)
-	{
-		var diff = currentMap.Difficulties[idx];
-		GetNode<Button>("Play").Visible = diff.Load(currentMap) == Error.Ok;
-		currentDifficulty = diff;
-		Game.LoadedMap = currentDifficulty;
-		Game.LoadedMapData = currentDifficulty.Data;
 	}
 	public override async void OnShow()
 	{
