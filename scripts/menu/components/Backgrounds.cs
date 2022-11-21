682a06ab0ebe98b59b801d054e01afff8d8e4bdb
jayden <46307433+krmeet@users.noreply.github.com>
2022-11-21 16:59:42 +0000

    increase matt chance

--- scripts/menu/components/Backgrounds.cs
+++ scripts/menu/components/Backgrounds.cs
@@ -40,7 +40,7 @@ public class Backgrounds : Control
 			AddChild(transition);
 			MoveChild(transition, 1);
 			var next = Textures[Mathf.PosMod(background++, Textures.Count)];
-			if (random.Next(0, 1337) == 69)
+			if (random.Next(0, 1337420) == 69)
 				next = Global.Matt;
 			Texture.Texture = next;
 			tween.StopAll();
